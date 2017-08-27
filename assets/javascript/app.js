7f319a30715b3cfb8f4ed1df3eb265b24384aeb6
annacrespo12 <annacrespo12@gmail.com>
2017-08-27 13:55:24 -0700

    buttons fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,3 @@
-
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=95bfdd6cda3f400aa1f2c12d2ba93f14";
-
-
-
 // $("button").on("click", function() {
 //     event.preventDefault();
 //     var x = $(this).data("input");
@@ -21,27 +16,61 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=95bf
 
 // submit form button needs to call a function that creates a new button via html append to body or parent div
 
+
 $("#create").on('click', function() {
-    event.preventDefault();
     let topic = document.getElementById("topic").value;
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=95bfdd6cda3f400aa1f2c12d2ba93f14";
-    console.log(topic);
-    $("#topicButtons").append("<button id=" + topic + ">" + topic + "</button>");
-    console.log(queryURL);
-    $.ajax({ url: queryURL, method: 'GET' })
-        .done(function(response) {
-            console.log(response.data[0].rating);
-            console.log(response.data);
+    $("#topicButtons").append("<button id=" + topic + " value=" + topic + ">" + topic + "</button>");
+    $("button").click(function() {
+        var fired_button = $(this).val();
+        console.log(fired_button);
+        event.preventDefault();
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=95bfdd6cda3f400aa1f2c12d2ba93f14";
+        console.log(queryURL);
+        $.ajax({ url: queryURL, method: 'GET' })
+            .done(function(response) {
+                console.log(response.data[0].rating);
+                console.log(response.data);
+                for (var i = 0; i < response.data.length; i++) {
+                    $('#GIFArea').prepend("<img src='" + response.data[i].images.downsized.url + "' >");
+                    $('#GIFArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
+                }
+            })
 
-            // $('body').append("<p>"+response.data[0].rating);
-            for (var i=0; i<response.data.length; i++) {
-        	$('#GIFArea').prepend("<img src='"+ response.data[i].images.downsized.url + "' >");
-        	$('#GIFArea').prepend("<p>Rating: "+response.data[i].rating+ "</p>");
-        }
-        })
-
-        
+    });
 })
+
+
+
+
+
+
+
+// $("#create").on('click', function() {
+// 	let topic = document.getElementById("topic").value;
+//     event.preventDefault();
+//     let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=95bfdd6cda3f400aa1f2c12d2ba93f14";
+//     // console.log(topic);
+//     $("#topicButtons").append("<button id=" + topic + " value=" + topic + ">" + topic + "</button>");
+//     console.log(queryURL);
+//     $.ajax({ url: queryURL, method: 'GET' })
+//         .done(function(response) {
+//             console.log(response.data[0].rating);
+//             console.log(response.data);
+
+//             // $('body').append("<p>"+response.data[0].rating);
+//             for (var i = 0; i < response.data.length; i++) {
+//                 $('#GIFArea').prepend("<img src='" + response.data[i].images.downsized.url + "' >");
+//                 $('#GIFArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
+//             }
+//         })
+//     $("button").click(function() {
+//         var fired_button = $(this).val();
+//         console.log(fired_button);
+//     });
+
+// })
+
+
 
 // submit form input needs to be stored in a variable
 // create new function needs to pass in variable of submit form similar to wins and losses on other game
